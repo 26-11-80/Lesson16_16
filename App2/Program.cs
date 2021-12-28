6396b77a5fe5fc387fd074279092e1f181e57b25
anvarovagulnaz <anvarovagulnaz@gmail.com>
2021-12-28 22:48:59 +0500

    Добавьте файлы проекта.

---
+++ App2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Lesson16_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxEmployee = products[0];
+            foreach (Product e in products)
+            {
+                if (e.Price > maxEmployee.Price)
+                {
+                    maxEmployee = e;
+                }
+            }
+            Console.WriteLine($"{maxEmployee.Code} {maxEmployee.Name} {maxEmployee.Price}");
+            Console.ReadKey();
+        }
+    }
+}
